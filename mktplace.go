ee8f282d9f5e5944d4c71bd2cd207af019b74228
bsvcs2016 <bsvcw.2016@gmail.com>
2016-11-18 15:34:15 -0500

    Update mktplace.go

--- mktplace.go
+++ mktplace.go
@@ -1,5 +1,5 @@
 /*
-Chain code for Testing Blockchain
+Chain code for Test
 */
 
 package main
@@ -9,11 +9,34 @@ import (
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"errors"
 	"encoding/json"
-	//"strconv"
-	//"crypto/x509"
+	"encoding/pem"
+	"strings"
+	"crypto/x509"
 	"strconv"
 	//"time"
+	"net/url"
 )
+
+//==============================================================================================================================
+//	 Participant types - Each participant type is mapped to an integer which we use to compare to the value stored in a
+//						 user's eCert
+//==============================================================================================================================
+//CURRENT WORKAROUND USES ROLES CHANGE WHEN OWN USERS CAN BE CREATED SO THAT IT READ 1, 2, 3, 4
+const   ISSUER  =  1
+const   BANK   =  2
+const   INVESTOR =  3
+const   REGBODY  =  4
+
+
+//==============================================================================================================================
+//	 Status types - Asset lifecycle is broken down into 5 statuses, this is part of the business logic to determine what can 
+//					be done to the vehicle at points in it's lifecycle
+//==============================================================================================================================
+const   STATE_INITIAL  			=  0
+const   STATE_PARTIAL  			=  1
+const   STATE_FULL 		=  2
+const   STATE_PAID_OUT 			=  3
+
 
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
@@ -25,6 +48,7 @@ type Issuer struct{
  IssuerAddress string
  IssuerCreditRating string
  InstrumentList []string
+ TradeList []string
 }
 
 type Bank struct {
@@ -32,6 +56,8 @@ BankID string
 BankName string
 BankAddress string
 BankTier string
+InstrumentList []string
+TradeList []string
 }
 
 type Investor struct {
@@ -39,6 +65,8 @@ InvestorID string
 InvestorName string
 InvestorAddress string
 InvestorCreditRating string
+InstrumentList []string
+TradeList []string
 }
 
 type Regulator struct {
@@ -48,19 +76,31 @@ type Transaction struct {
 TradeID  string
 InstrumentID string
 BankID string
+IssuerID string
+InvestorID string
+Notional float64
+Rate float64
 }
 
 type Instrument struct {
 InstrumentID string
-Type string
-Maturity string //time.Time
-Coupon float64
+//Type string
+Coupon string
+Units int
 Notional float64
 IssueDate string //time.Time
+Maturity string //time.Time
 IssuerID string
-BookedNotional float64 
+Callable string
+Description string
 }
 
+/*type Couponpay struct{
+}
+
+type Maturitypay struct{
+}
+*/
 
 func main() {
 	err := shim.Start(new(SimpleChaincode))
@@ -71,9 +111,9 @@ func main() {
 
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-
+	// Initialize Two Issuer
 	issuer:= Issuer{		 
-	IssuerID: "Test1",	   
+	IssuerID: "ISS-1",	   
 	IssuerName:	"Issuer 1", 
 	IssuerAddress: "Client", 
 	IssuerCreditRating: "A", 
@@ -112,17 +152,18 @@ if err == nil {
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("invoke is running " + function)
 
+	//caller, caller_affiliation, err := t.get_caller_data(stub)
+	
 	// Handle different functions
 	if function == "init" {
 		return t.Init(stub, "init", args)
 	} else if function == "createIssue" {
 		return t.createIssue(stub, args)
-	} else if function == "invest" {
+	} else if function == "bankResponse" {
 		return t.write(stub, args)
+	} else if function == "investorResponse" {
+		return t.write(stub, args)		
 	}
-
-	fmt.Println("invoke did not find func: " + function)
-
 	return nil, errors.New("Received unknown function invocation: " + function)
 }
 
@@ -162,8 +203,8 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 func (t *SimpleChaincode) createIssue(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var transactionID string
 	var err error
-	var vtype, vMaturity,  vIssueDate, vIssuerID string
-	var vNotional , vCoupon float64
+	var vMaturity,  vIssueDate, vIssuerID, vCoupon string
+	var vNotional  float64
 	fmt.Println("running createIssue")
 
 	if len(args) != 5 {
@@ -182,12 +223,10 @@ func (t *SimpleChaincode) createIssue(stub shim.ChaincodeStubInterface, args []s
 	tid = tid + 1 
 	transactionID = "trans"+strconv.Itoa(tid) 
 
-	vtype = args[0] //type
+	//vtype = args[0] //type
 	vMaturity = args[1] 
-	vCoupon, err1 := strconv.ParseFloat(args[2],64)
-	if err1 != nil {
-	return nil, errors.New("Invalid number in Argument 3") 
-	}
+	vCoupon  = args[2]
+	
 	vNotional, err2  := strconv.ParseFloat(args[3],64)
 	if err2 != nil {
 	return nil, errors.New("Invalid number in Argument 4") 
@@ -197,7 +236,6 @@ func (t *SimpleChaincode) createIssue(stub shim.ChaincodeStubInterface, args []s
 	
 	inst := Instrument{
 	InstrumentID:transactionID,
-	Type:vtype,
 	Maturity : vMaturity,
 	Coupon:vCoupon,
 	Notional:vNotional,
@@ -258,4 +296,100 @@ func updateIssueHistory(stub shim.ChaincodeStubInterface, issuerID string, instr
 		return errors.New("Error while updating Issuer status")
 	}
 	return nil
+}
+
+
+//==============================================================================================================================
+//	 General Functions
+//==============================================================================================================================
+//	 get_ecert - Takes the name passed and calls out to the REST API for HyperLedger to retrieve the ecert
+//				 for that user. Returns the ecert as retrived including html encoding.
+//==============================================================================================================================
+func (t *SimpleChaincode) get_ecert(stub shim.ChaincodeStubInterface, name string) ([]byte, error) {
+	
+	ecert, err := stub.GetState(name)
+
+	if err != nil { return nil, errors.New("Couldn't retrieve ecert for user " + name) }
+	
+	return ecert, nil
+}
+
+//==============================================================================================================================
+//	 add_ecert - Adds a new ecert and user pair to the table of ecerts
+//==============================================================================================================================
+
+func (t *SimpleChaincode) add_ecert(stub shim.ChaincodeStubInterface, name string, ecert string) ([]byte, error) {
+	
+	
+	err := stub.PutState(name, []byte(ecert))
+
+	if err == nil {
+		return nil, errors.New("Error storing eCert for user " + name + " identity: " + ecert)
+	}
+	
+	return nil, nil
+
+}
+//==============================================================================================================================
+//	 get_caller - Retrieves the username of the user who invoked the chaincode.
+//				  Returns the username as a string.
+//==============================================================================================================================
+
+func (t *SimpleChaincode) get_username(stub shim.ChaincodeStubInterface) (string, error) {
+
+	bytes, err := stub.GetCallerCertificate();
+	if err != nil { return "", errors.New("Couldn't retrieve caller certificate") }
+	x509Cert, err := x509.ParseCertificate(bytes);				// Extract Certificate from result of GetCallerCertificate						
+															if err != nil { return "", errors.New("Couldn't parse certificate")	}
+															
+	return x509Cert.Subject.CommonName, nil
+}
+
+
+//==============================================================================================================================
+//	 get_caller_data - Calls the get_ecert and check_role functions and returns the ecert and role for the
+//					 name passed.
+//==============================================================================================================================
+
+func (t *SimpleChaincode) get_caller_data(stub shim.ChaincodeStubInterface) (string, int, error){	
+
+	user, err := t.get_username(stub)
+		if err != nil { return "", -1, err }
+
+	ecert, err := t.get_ecert(stub, user);					
+		if err != nil { return "", -1, err }
+
+	affiliation, err := t.check_affiliation(stub,string(ecert));			
+		if err != nil { return "", -1, err }
+
+	return user, affiliation, nil
+}
+
+
+//==============================================================================================================================
+//	 check_affiliation - Takes an ecert as a string, decodes it to remove html encoding then parses it and checks the
+// 				  		certificates common name. The affiliation is stored as part of the common name.
+//==============================================================================================================================
+
+func (t *SimpleChaincode) check_affiliation(stub shim.ChaincodeStubInterface, cert string) (int, error) {																																																					
+	
+
+	decodedCert, err := url.QueryUnescape(cert);    				// make % etc normal //
+	
+	if err != nil { return -1, errors.New("Could not decode certificate") }
+	
+	pem, _ := pem.Decode([]byte(decodedCert))           				// Make Plain text   //
+
+	x509Cert, err := x509.ParseCertificate(pem.Bytes);				// Extract Certificate from argument //
+														
+	if err != nil { return -1, errors.New("Couldn't parse certificate")	}
+
+	cn := x509Cert.Subject.CommonName
+	
+	res := strings.Split(cn,"\\")
+	
+	affiliation, _ := strconv.Atoi(res[2])
+	
+	return affiliation, nil
+		
 }
