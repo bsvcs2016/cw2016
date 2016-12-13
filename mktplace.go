b29a400bddb6d689e20a9dafbe875d2c829d0968
bsvcs2016 <bsvcw.2016@gmail.com>
2016-12-13 13:00:19 -0500

    Update mktplace.go

--- mktplace.go
+++ mktplace.go
@@ -91,7 +91,7 @@ func main() {
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	// initialize Instruent	
 	instrument:= Instrument{		
-		Symbol :"ISU90D10BPS",
+		Symbol :"ISU90D",
 	Coupon :"BW",
 	Quantity :10000,
 	InstrumentPrice :100,
@@ -111,7 +111,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	}
 	
 	instrument1:= Instrument{		
-		Symbol :"ISU90DCALL",
+		Symbol :"ISU120CALL",
 	Coupon :"M",
 	Quantity :50000,
 	InstrumentPrice :50,
